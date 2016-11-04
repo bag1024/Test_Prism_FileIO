d0eafb2cf3873234e2c2fcfface7cd497980d02a
Sung Yung <bag1024@msn.com>
2016-11-04 10:44:19 +0800

    Fast

---
+++ Test_Prism_FileIO/Test_Prism_FileIO/Test_Prism_FileIO/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Test_Prism_FileIO.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
